ff8f37d365a3fc0c5edea82b2da721ba7fa609af
fuku <>
2023-04-07 12:00:16 +0900

    ポート番号を3000→50000に変更 簡単なarticleのapiを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,17 @@
 class ArticlesController < ApplicationController
   def index
-    render json: { message: 'Hello World!' }
+    articles = Article.all
+    render json: articles
   end
+
+  def create
+    article = Article.create(title: params[:title], text: params[:text])
+    render json: { article: }
+  end
+
+  # private
+
+  # def article_params
+  #   params.require(:article).permit(:title, :text)
+  # end
 end
