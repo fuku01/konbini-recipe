7f4de88872a0fdc6d71f27370c83f06bb5cb5787
fuku <>
2023-05-01 21:49:25 +0900

    fix config

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Myapp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.hosts << "api.konbini-recipe.link"
+
     # CORSの設定を追加
     config.middleware.insert_before 0, Rack::Cors do
       allow do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,6 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.hosts << "api.konbini-recipe.link"
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
