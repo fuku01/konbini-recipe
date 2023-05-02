4aa330e173d6cad6cab6513a0553703897874329
fuku <>
2023-05-02 21:50:51 +0900

    レシピ投稿・編集の際に、写真・タイトル・作り方を必須に修正。 #14 #16

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :barcodetags
   has_many :favorites
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :image, presence: true
 end
