64a26e83a74163b2d673e9a60e8b398f4bdf8d32
fuku <>
2023-05-03 19:11:06 +0900

    fix-recipe-validates-length

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,33 @@ class ApplicationController < ActionController::API
     # 認証失敗
     render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
   end
+
+  # def authenticate_user
+  #   id_token = request.headers['Authorization']&.sub(/^Bearer /, '') # JWTトークンのペイロードを検証
+  #   decoded_token = verify_firebase_token(id_token)
+  #   if decoded_token
+  #     # トークンが正常に検証された場合、ログインしているユーザーの情報を返す
+  #     @current_user = User.find_by(firebase_uid: decoded_token['user_id'])
+  #   end
+  #   # トークンが無効な場合、エラーを返す
+  #   render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+  # end
+
+  # def verify_firebase_token(token)
+  #   url = URI('https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com')
+  #   response = Net::HTTP.get(url)
+  #   certificates = JSON.parse(response)
+  #   begin
+  #     decoded_token = JWT.decode(token, nil, false, { jwks: certificates })
+  #     # トークンの有効期限を確認して、有効期限が切れていない場合は、ペイロード部分を返す
+  #     if decoded_token[0]['exp'] >= Time.now.to_i
+  #       return decoded_token[0]
+  #     else
+  #       return nil
+  #     end
+  #     # ペイロード部分を返します。
+  #   rescue JWT::DecodeError => e
+  #     nil
+  #   end
+  # end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_many :favorites
 
   validates :title, presence: true, length: { maximum: 40 }
-  validates :content, presence: true, length: { maximum: 300 }
+  validates :content, presence: true, length: { maximum: 1000 }
   validates :image, presence: true
   validates :calorie, length: { maximum: 9999 }
   validates :price, length: { maximum: 9999 }
