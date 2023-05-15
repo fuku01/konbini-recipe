3b03cbad819716bf1f05597013340e6939013332
fuku <>
2023-05-15 12:02:22 +0900

    ユーザーがいいね登録したレシピ一覧を取得するアクションを新規作成。 #52

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,15 @@ Rails.application.routes.draw do
   delete '/users/:id', to: 'users#destroy'
 
   # Recipes
-  get '/recipes', to: 'recipes#index'
-  get '/new_recipes', to: 'recipes#show_new_recipes'
-  get '/my_recipes', to: 'recipes#show_my_recipes'
-  post '/recipes', to: 'recipes#create'
-  get '/recipes/:id', to: 'recipes#show'
-  put '/recipes/:id', to: 'recipes#update'
-  patch '/recipes/:id', to: 'recipes#update'
-  delete '/recipes/:id', to: 'recipes#destroy'
+  get '/recipes', to: 'recipes#index' # 全てのレシピを作成日の降順で取得し、JSON形式で返す
+  get '/new_recipes', to: 'recipes#show_new_recipes' # 新着レシピを作成日の降順で取得し、JSON形式で返す
+  get '/my_recipes', to: 'recipes#show_my_recipes' # ログイン中のユーザーのレシピを作成日の降順で取得し、JSON形式で返す
+  get '/favorite_recipes', to: 'recipes#show_favorite_recipes' # ログイン中のユーザーのお気に入りレシピを作成日の降順で取得し、JSON形式で返す
+  post '/recipes', to: 'recipes#create' # 指定されたレシピを作成し、JSON形式で返す
+  get '/recipes/:id', to: 'recipes#show' # 指定されたIDのレシピを取得し、JSON形式で返す
+  put '/recipes/:id', to: 'recipes#update' # 指定されたIDのレシピを更新し、JSON形式で返す
+  patch '/recipes/:id', to: 'recipes#update' # 指定されたIDのレシピを更新し、JSON形式で返す
+  delete '/recipes/:id', to: 'recipes#destroy' # 指定されたIDのレシピを削除する
 
   # tags
   get '/tags', to: 'tags#index'
@@ -28,26 +29,9 @@ Rails.application.routes.draw do
 
   # Favorites
   get '/favorites', to: 'favorites#index'
-  post '/favorites', to: 'favorites#create'
-  get '/favorites/:id', to: 'favorites#show'
-  get '/isRecipe_favorite/:recipe_id', to: 'favorites#show_isRecipe_favorite'
-  get '/favorite_count/:recipe_id', to: 'favorites#show_favorite_count'
-  get '/my_favorites', to: 'favorites#show_my_favorites'
-  put '/favorites/:id', to: 'favorites#update'
-  patch '/favorites/:id', to: 'favorites#update'
-  delete '/favorites/:id', to: 'favorites#destroy'
-
-  # index
-  get '/todos', to: 'todos#index'
-  # show
-  get '/todos/:id', to: 'todos#show'
-  # create
-  post '/todos', to: 'todos#create'
-  # update
-  # put '/todos/:id', to: 'todos#update'
-  patch '/todos/:id', to: 'todos#update'
-  # delete
-  delete '/todos/:id', to: 'todos#destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
+  post '/favorites', to: 'favorites#create' # 指定されたレシピをお気に入り登録する
+  get '/favorites/:id', to: 'favorites#show' # 指定されたIDのお気に入りを取得し、JSON形式で返す
+  get '/isRecipe_favorite/:recipe_id', to: 'favorites#show_isRecipe_favorite' # ログイン中のユーザーが指定されたレシピをお気に入り登録しているかどうかを取得
+  get '/favorite_count/:recipe_id', to: 'favorites#show_favorite_count' # 指定されたレシピのお気に入り数を取得し、JSON形式で返す
+  delete '/favorites/:id', to: 'favorites#destroy' # 指定されたIDのお気に入りを削除する
 end
